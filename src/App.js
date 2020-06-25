f7c21b28014057622163923a9179ba5d8ae9c10c
therakeshpurohit <rakeshpurohit909@gmail.com>
2020-06-25 17:47:12 +0530

    Refs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import RefsDemo from './components/RefsDemo';
 
 function App() {
   return (
     <div className="App">
+      <RefsDemo />
       {/* <ParentComp /> */}
       {/* <Tables/> */}
       {/* <FragmentDemo /> */}
