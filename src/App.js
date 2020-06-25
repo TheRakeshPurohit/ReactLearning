e726fd4809205f0d775aa8ebec5b1006bf0cda9b
therakeshpurohit <rakeshpurohit909@gmail.com>
2020-06-25 11:02:34 +0530

    Method as props child to parent vice versa

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
+// import Message from './components/Message';
+// import Counter from './components/Counter';
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <FunctionClick />
+      <ParentComponent />
+      {/* <FunctionClick />
       <ClassClick />
-      <EventBind />
+      <EventBind /> */}
     {/* <Counter />
     {/* <Message /> */}
 {/* 
